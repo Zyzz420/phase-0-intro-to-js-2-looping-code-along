4e16831300f215bdd6b3f83116802325636835ee
Abdulaziz <abdulaziz.komara@student.moringaschool.com>
2023-06-22 11:09:25 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-yabelbabel
+function countDown(){
+    let countDown=0
+    while(countDown<11)
+    console.log(countDown++)
+}
+const names =["Guadalupe","Ollie","Aki"]
+
+function writeCards(names,event){
+    return names.map(name=>`Thank you, ${name}, for the wonderful ${event} gift!`)
+}
+console.log(writeCards(names,"surprise"))    
+
